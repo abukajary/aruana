7c79a3b7f7ac16f0aca8d717f95140019ad21297
abukajary <jarylgapov.abubakir@gmail.com>
2025-03-15 13:24:03 +0500

    777

---
+++ src/locales/about.ts
@@ -0,0 +1,58 @@
+const about = {
+    adults: {
+      title: {
+        kk: "Көп бейінді медициналық оңалту орталығы «Аруана»",
+        ru: "Многопрофильный центр медицинской реабилитации «Аруана»",
+        en: "Multidisciplinary Medical Rehabilitation Center 'Aruana'",
+      },
+      description: {
+        kk: "«Аруана» - Қазақстандағы инсульттан, омыртқа мен буындарға операциялардан, тірек-қимыл аппараты жарақатынан және жүрекке операциялардан кейін оңалту көмегін дәлелді медицина негізінде көрсететін алғашқы бірегей орталық.",
+        ru: "«Аруана» - это первый уникальный центр в Казахстане, который оказывает реабилитационную помощь пациентам, перенесшим инсульт, операции на позвоночнике и суставах, травматические повреждения опорно-двигательного аппарата и кардиохирургические вмешательства, на основе доказательной медицины.",
+        en: "'Aruana' is the first unique center in Kazakhstan providing rehabilitation assistance to patients recovering from strokes, spinal and joint surgeries, musculoskeletal injuries, and cardiac surgeries based on evidence-based medicine.",
+      },
+      mission: {
+        kk: "Жобаның миссиясы – адамдарға денсаулығын қалпына келтіруге, өмір сапасын жақсартуға және ауруға дейінгі өміріне оралуға көмектесу.",
+        ru: "Миссия проекта – помочь людям восстановить здоровье, улучшить качество жизни и вернуться к прежнему образу жизни до болезни.",
+        en: "The mission of the project is to help people restore their health, improve their quality of life, and return to their pre-illness lifestyle.",
+      },
+      features: {
+        kk: "Орталықтың ерекшелігі – инсульттан кейінгі оңалту көмегін ерте кезеңнен бастап үздіксіз көрсету.",
+        ru: "Особенностью центра является непрерывность и преемственность оказания реабилитационной помощи пациентам после перенесенного инсульта на ранних этапах после острого периода.",
+        en: "A key feature of the center is the continuous and consistent rehabilitation care for patients after a stroke, starting from the early stages following the acute period.",
+      },
+      services: {
+        list: {
+          kk: [
+            "Бас-ми және жұлын жарақаттарының салдары.",
+            "Бас және жұлын миына жасалған операциялардың салдары.",
+            "Шеткері жүйке жүйесінің жарақаты.",
+          ],
+          ru: [
+            "С последствиями черепно-мозговых и спинальных травм.",
+            "С последствиями операций на головном и спинном мозге.",
+            "С травмой периферической нервной системы.",
+          ],
+          en: [
+            "With the consequences of traumatic brain and spinal injuries.",
+            "With the consequences of surgeries on the brain and spinal cord.",
+            "With peripheral nervous system injuries.",
+          ],
+        },
+      },
+      contacts: {
+        address: {
+          kk: "Алматы қ., Алмалы көшесі, 35а",
+          ru: "г.Алматы, ул.Алмалы 35а",
+          en: "Almaty, Almaly Street 35a",
+        },
+        phone: {
+          kk: "+7-(727) 262-12-08; +7 702 944-43-76",
+          ru: "+7-(727) 262-12-08; +7 702 944-43-76",
+          en: "+7-(727) 262-12-08; +7 702 944-43-76",
+        },
+      },
+    },
+  };
+  
+  export default about;
+  
